f46038f89ae2788aa3e4ed2fffa751a9749feffc
panospyrakis <panospurakis@gmail.com>
2018-03-21 23:12:30 +0200

    minor fix

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -30,6 +30,9 @@ public class DetailActivity extends AppCompatActivity {
     @BindView(R.id.description_tv)
     TextView descriptionTv;
 
+    @BindView(R.id.image_iv)
+    ImageView ingredientsIv;
+
     private Sandwich sandwich;
 
 
@@ -38,7 +41,7 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        ingredientsIv = findViewById(R.id.image_iv);
 
         Intent intent = getIntent();
         if (intent == null) {
